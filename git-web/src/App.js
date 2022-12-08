3a16f3f5cf8628b3b2366709a56a74d2b506a9ad
Amitjoshi1991 <amitjoshi1091@gmail.com>
2022-12-08 17:10:51 +0530

    fixes

--- git-web/src/App.js
+++ git-web/src/App.js
@@ -1,37 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react"
 import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
- revert-2-dev
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <h1>click to Add</h1>
- abhi
-       <text> hello brother </text>
-
-      <h1>heloo </h1>
-
-      <Button variant="text" color="primary">
-        Add
-      </Button>
- dev
- dev
+      <h1>Hello Amit</h1>
+      <button>Click to add the Data</button>
     </div>
   );
 }
